635c1b8461b8d9fd0e38aef2d727b946abd8ff2e
okdol1 <okdol0611@gmail.com>
2023-12-12 22:39:44 +0900

    Implement custom Dayjs utility function

--- app/components/PostItem.js
+++ app/components/PostItem.js
@@ -1,6 +1,7 @@
-import { formatDate } from "/utils/formatDate.js";
+import dayjs from "/utils/dayjs.js";
 
 const PostItem = ({ post, href }) => {
+  const formattedDate = dayjs(post.date).format("YYYY-MM-DD");
   return `
     <li class="post-item">
       <a href="${href}">
@@ -8,7 +9,7 @@ const PostItem = ({ post, href }) => {
         <div>
           <h2 class="post-title">${post.title}</h2>
           <p class="post-summary">${post.summary}</p>
-          <p class="post-date">${formatDate(post.date)}</p>
+          <p class="post-date">${formattedDate}</p>
         </div>
       </a>
     </li>
